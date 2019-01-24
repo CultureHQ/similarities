e63685ece547cae7862409fd42f5672fcad8569f
Kevin Deisz <kevin.deisz@gmail.com>
2019-01-24 11:47:22 -0500

    Fix up lint violations

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     path: path.resolve(__dirname, "docs"),
     filename: "[name].js"
   },
-  entry: path.join(__dirname, "src", "index.js"),
+  entry: path.join(__dirname, "src", "App.js"),
   module: {
     rules: [
       { test: /\.js$/, use: "babel-loader", exclude: /node_modules/ }
